b6313819343c50e8bc689ab872493b1c2635ea57
anyajin6-oss <anyajin6@gmail.com>
2026-02-18 01:18:47 +0800

    Fix Flask app initialization and main check

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Render! 部署成功啦！'
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
